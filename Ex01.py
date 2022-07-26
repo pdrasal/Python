91e60c7ecdec9a511e9f597e65f094d8f2606ebc
Patricio Drasal <pdrasal@hotmail.com>
2022-07-26 22:56:03 +0300

    dates

--- Ex01.py
+++ Ex01.py
@@ -1,5 +1,8 @@
 #this line uses double quotation mark because it has a single inside. Strings can be referenced indistinctl using double or single
+from calendar import day_abbr
 from logging import StringTemplateStyle
+
+from pendulum import today, yesterday
 
 name=input("What's your name \n")
 print ('hello ' +name) #strings concatenate with +
@@ -34,3 +37,19 @@ print (str(first_number) + ' is the first number') #to print convert
 first_num=input('Enter the first number')
 second_num=input ('Enter the second number')
 print (int(first_num)+int(second_num)) #in this case had to convert to int the input as was a string
+
+#dates
+
+from datetime import datetime,timedelta
+current_date=datetime.now()
+print('Today is ' + str(current_date))
+
+
+one_day= timedelta(days=1)
+yesterday= current_date - one_day
+print ('Yesterday was ' + str(yesterday))
+
+print ('Day :' + str(current_date.day))
+print ('Day :' + str(current_date.month))
+print ('Day :' + str(current_date.year))
+
