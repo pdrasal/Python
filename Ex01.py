e9c9f0cc64cbf79138e49e2f799a5e3980ce1332
Patricio Drasal <pdrasal@hotmail.com>
2022-07-28 23:05:54 +0300

    va

--- Ex01.py
+++ Ex01.py
@@ -64,3 +64,7 @@ birthday = input('When is your birthday (dd/mm/yyyy)? ')
 birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
 
 print ('Birthday: ' + str(birthday_date))
+
+
+#error handling
+
